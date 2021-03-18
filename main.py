c83f86dee6f2bb344ec5867363200f9db804a575
l-Il <0rky@pm.me>
2021-03-19 00:11:57 +0300

    1.0.0 Completed

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
+from random import shuffle
 from tkinter import *
 
 
 class Plot:
     def __init__(self):
-        self.A = []
         self.i = 0
-        self.color = ['#FFCC00', '#0000FF', '#00FF00', '#00FFFF', '#FF0000', '#FF00FF', '#FFFF00', '#FFCCFF', '#CCCCFF']
+        self.color = ['#8AEA92', '#177E89', '#585481', '#FFBA49', '#A4243B', '#F05D23', '#4D6CFA', '#31E981', '#FAA300']
+        shuffle(self.color)
         self.root = Tk()
         self.root.geometry('597x435')
         self.root.title('График:')
         self.t1 = Frame(self.root)
         self.canvas = Canvas(self.root, bg='#FFFFFF', width=400, height=400)
+        self.canvas_set()
+        Button(self.root, relief='groove', text='Перенести на график', command=self.place).place(x=7, y=410, width=130, height=20)
+        Button(self.root, relief='groove', text='Создать новую прямую', command=self.new).place(x=142, y=410, width=140, height=20)
+        self.new()
+        Button(self.root, relief='groove', text='Очистить всё', command=self.clear).place(x=287, y=410, width=120, height=20)
+        self.status = Label(self.root, text='Введите координаты:')
+        self.status.place(x=412, y=410, width=180)
+        self.root.mainloop()
+
+    def canvas_set(self):
+        self.canvas = Canvas(self.root, bg='#FFFFFF', width=400, height=400)
         # Сетка, одна клетка = 20х20
-        k = 0
         for x in range(22):  # Х - переменная обхода
-            self.canvas.create_line(k, 400, k, 0, fill='gray')
-            self.canvas.create_line(0, k, 400, k, fill='gray')
-            k = 20 * x
-            print(k)
+            self.canvas.create_line(20 * x, 400, 20 * x, 0, fill='gray')
+            self.canvas.create_line(0, 20 * x, 400, 20 * x, fill='gray')
         # Оси
         self.canvas.create_line(400, 200, 0, 200, arrow=FIRST, fill='black', width=2)  # X
         self.canvas.create_line(200, 0, 200, 400, arrow=FIRST, fill='black', width=2)  # Y
         self.canvas.place(x=5, y=5)
-        Button(self.root, relief='groove', text='Создать новую прямую', command=self.new).place(x=5, y=410, width=140, height=20)
-        self.new()
-        Button(self.root, relief='groove', text='Перенести на график', command=self.place).place(x=150, y=410, width=130, height=20)
-        Button(self.root, relief='groove', text='Очистить всё', command=self.clear).place(x=285, y=410, width=122, height=20)
-
-        self.root.mainloop()
 
     def new(self):
         if self.i < 9:
@@ -50,17 +53,24 @@ class Plot:
             pass
 
     def place(self):
-        self.A = []
-        for x in range(self.i):
-            exec(f'self.A.append([self.entry_x0{x}.get(), self.entry_y0{x}.get(), self.entry_x1{x}.get(), self.entry_y1{x}.get()])')
-            exec(f'''self.canvas.create_line(self.entry_x0{x}.get(), 
-                                            self.entry_y0{x}.get(), 
-                                            self.entry_x1{x}.get(), 
-                                            self.entry_y1{x}.get(), width=2, fill=self.color[x])''')
-        print(self.A)
+        try:
+            for x in range(self.i):
+                exec(f'''self.canvas.create_line(200+float(self.entry_x0{x}.get())*20, 
+                                                200-float(self.entry_y0{x}.get())*20, 
+                                                200+float(self.entry_x1{x}.get())*20, 
+                                                200-float(self.entry_y1{x}.get())*20, width=2, fill=self.color[x])''')
+        except Exception as _:
+            pass
 
     def clear(self):
-        pass
+        self.canvas.destroy()
+        self.canvas_set()
+        for x in range(self.i):
+            exec(f'self.t{x}.destroy()')
+        self.i = 0
+        self.new()
+        self.status.config(text='Очищено.')
+        shuffle(self.color)
 
 
 if __name__ == '__main__':
